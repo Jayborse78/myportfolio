13559ae184b2acb216ea74cd8f71c294e8ed2644
Jayesh Borse <101235440+Jayborse78@users.noreply.github.com>
2023-07-08 12:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// console.log("start")
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "FullStack Developer", "Web Designer", "Backend Developer", "Frontend Developer"],
+    typeSpeed: 70,
+    BackSpeed: 30,
+    loop: true
+})
